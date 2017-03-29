d92a19e2edbc488dfdf9535318175ef2e065d2c8
Supakorn Thongtra <rainstorm.armiie@gmail.com>
2017-03-29 16:08:25 +0700

    Import bulma

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
 
 
 const appRoutes: Routes = [
