96ce35001243d99d88b66bcca150b70e3229ac62
Supakorn Thongtra <ARM@Supakorns-MacBook-Pro.local>
2017-04-04 22:50:15 +0700

    add cinemagraph add font add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import { AppComponent } from './app.component';
 
 import { routing } from './app.routing';
 
-import { TypingCarouselComponent } from './typing-carousel/typing-carousel.component';
+import { TypingCarouselComponent } from './shared/typing-carousel/typing-carousel.component';
 import { TypingCarouselDirective } from './shared/directive/typing-carousel.directive';
-import { MobileTestComponent } from './mobile-test/mobile-test.component';
+import { MobileTestComponent } from './shared/mobile-test/mobile-test.component';
 import { MobileHideDirective } from './shared/directive/mobile-hide.directive';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PacmanComponent } from './pacman/pacman.component';
-import { SupermanLoadingComponent } from './superman-loading/superman-loading.component';
+import { PacmanComponent } from './shared/pacman/pacman.component';
+import { SupermanLoadingComponent } from './shared/superman-loading/superman-loading.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +34,11 @@ import { FooterComponent } from './footer/footer.component';
     SupermanLoadingComponent,
     HomeComponent,
     FooterComponent,
+    AboutmeComponent,
+    ContactComponent,
+    ExperienceComponent,
+    ProjectComponent,
+    TechnologyComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  toggleStatus = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.toggleStatus = !this.toggleStatus;
   }
 
 }
