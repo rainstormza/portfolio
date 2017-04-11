d09e874b1a4f2980b482421fb4bcb926db83bfdc
Supakorn Thongtra <ARM@Supakorns-MacBook-Pro.local>
2017-04-12 00:13:42 +0700

    add bg to all component add template html add more cinema graph

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { PageScrollConfig } from 'ng2-page-scroll';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { PageScrollInstance, PageScrollService, EasingLogic, PageScrollConfig } from 'ng2-page-scroll';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +11,8 @@ export class NavbarComponent implements OnInit {
 
   toggleStatus = false;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private document: any,
+                     private pageScrollService: PageScrollService) {
         PageScrollConfig.defaultScrollOffset = 0;
         PageScrollConfig.defaultEasingLogic = {
             ease: (t: number, b: number, c: number, d: number): number => {
@@ -27,8 +29,29 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+
+
   toggle() {
     this.toggleStatus = !this.toggleStatus;
   }
+
+  scrollTop() {
+    // window.scrollTo(0, 0);
+    let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, '#home');
+    this.pageScrollService.start(pageScrollInstance);
+  }
+
+  scrollOther(name: string) {
+    console.log(name);
+    let pageScrollInstance: PageScrollInstance = PageScrollInstance.newInstance({
+          document: this.document,
+          scrollTarget: name,
+          pageScrollOffset: 64,
+          pageScrollInterruptible: false});
+    this.pageScrollService.start(pageScrollInstance);
+
+
+  }
+
 
 }
